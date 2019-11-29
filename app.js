ecacb7e5cb7ab470b4e883d65a90710db61b366b
RMH <rory.hennessey@gmail.com>
2019-11-29 11:25:42 -0500

    working with the edit state

--- app.js
+++ app.js
@@ -185,7 +185,7 @@ const App = (function (ItemCtrl, UICtrl) {
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
     // Edit icon click
-    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit)
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick)
   }
 
   // Add item submit
@@ -215,8 +215,8 @@ const App = (function (ItemCtrl, UICtrl) {
     e.preventDefault();
   }
 
-  // Update item submit
-  const itemUpdateSubmit = function (e) {
+  // Click edit item
+  const itemEditClick = function (e) {
     if (e.target.classList.contains('edit-item')) {
       // Get list item id (item-0, item-1, etc)
       const listId = e.target.parentNode.parentNode.id;
